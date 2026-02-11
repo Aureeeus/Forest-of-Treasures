252b20557584ff005869945ad0e9c81840abc12c
jadi <johndavidlibrado@gmail.com>
2026-02-12 01:24:18 +0800

    Added atlas design to skin memory and super.dispose() to handle freeing resources once application closes.

--- core/src/main/java/edu/tip/forestoftreasures/GameLauncher.java
+++ core/src/main/java/edu/tip/forestoftreasures/GameLauncher.java
@@ -2,6 +2,7 @@ package edu.tip.forestoftreasures;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 import edu.tip.forestoftreasures.View.MainMenuScreen;
@@ -12,12 +13,13 @@ public class GameLauncher extends Game {
 
     @Override
     public void create() {
-        skin = new Skin(Gdx.files.internal("ui/fotskin.json"));
+        skin = new Skin(Gdx.files.internal("ui/fotskin.json"), new TextureAtlas("ui/fotskin.atlas"));
         this.setScreen(new MainMenuScreen(this, skin));
     }
 
   @Override
   public void dispose() {
+    super.dispose();
     skin.dispose();
   }
 }
