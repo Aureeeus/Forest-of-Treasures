c381a09cc6704120e9900a18336e8b779b057233
jadi <johndavidlibrado@gmail.com>
2026-02-18 23:28:31 +0800

    Added initial content to Day1Screen. DrawableMaker utility class now makes a drawable for border and content background designs.

--- core/src/main/java/edu/tip/forestoftreasures/Controller/Day1Controller.java
+++ core/src/main/java/edu/tip/forestoftreasures/Controller/Day1Controller.java
@@ -1,11 +1,20 @@
 package edu.tip.forestoftreasures.Controller;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.github.tommyettinger.textra.Font;
+import com.github.tommyettinger.textra.TypingAdapter;
+import com.github.tommyettinger.textra.TypingLabel;
 
 import edu.tip.forestoftreasures.GameLauncher;
 import edu.tip.forestoftreasures.View.Day1Screen;
@@ -14,18 +23,40 @@ public class Day1Controller {
   private final GameLauncher game;
   private final Day1Screen screen;
 
+  // Important game-related variables
+  private final Queue<String> dialogueQueue = new LinkedList<>();
+  private boolean isTyping = false; // flag to check for dialogue box
+
+  // UI resources
   private Image settingsIcon;
+  private final Table scenarioContentTable;
+  private final Table textDialogueTable;
+  private final Table dialogueWidgetTable;
+
+  // Textures and Fonts
+  private Texture scenarioTexture;
+  private final Font textFont;
 
   public Day1Controller(GameLauncher game, Day1Screen screen) {
     this.game = game;
     this.screen = screen; 
 
-    settingsIcon = screen.getSettingsIcon();
+    this.scenarioContentTable = screen.getScenarioContentTable();
+    this.textDialogueTable = screen.getTextDialogueTable();
+    this.dialogueWidgetTable = screen.getDialogueWidgetTable();
+    this.settingsIcon = screen.getSettingsIcon();
+
+    // Convert BitmapFont to TextraTypist Font
+    BitmapFont baseFont = this.game.assets.get("fonts/DotGothic16-Dialogue.fnt", BitmapFont.class);
+    this.textFont = new Font(Gdx.files.internal("fonts/DotGothic16-Dialogue.fnt"));
 
     addListeners();
+    playScenario();
   }
 
+  // Function to add listener for widgets
   private void addListeners() {
+    // Event listener for settings icon
     screen.getSettingsIcon().addListener(new ClickListener() {
       // Event handler for when the settings icon is clicked
       @Override
@@ -65,5 +96,61 @@ public class Day1Controller {
         super.exit(event, x, y, pointer, toActor);
       }
     });
+  }
+
+  // Scenario initializer when game starts
+  private void playScenario() {
+    // Load intial image to the scenario box
+    scenarioTexture = new Texture(Gdx.files.internal("scenarios/day1/forest_intro.png"));
+    Image scenarioImage = new Image(scenarioTexture);
+    scenarioContentTable.add(scenarioImage).expand().fill();
+
+    // Load initial story lines to the dialogue box
+    addDialogue("The forest seems normal. Goblins, bugs, and fairies roam the forest. Nothing out of the ordinary. The forest is still lit by the sun - probably because its high noon and the trees, so far, are normal.");
+    addDialogue("{WAIT=1}\nA sprite lands on my shoulder. Rumors say these sprites have been blessed, and their touch can grant blessings or take them away from me. Should I shoo it away?");
+  }
+
+  // Adds text dialogue to textDialogueTable
+  private void addDialogue(String text) {
+    dialogueQueue.add(text);
+    if (!isTyping) showNextDialogue();
+  }
+
+  private void showNextDialogue() {
+    // Check if there is no queued text to display
+    if (dialogueQueue.isEmpty()) {
+      isTyping = false;
+      return;
+    }
+
+    isTyping = true;
+    String text = dialogueQueue.poll();
+
+    TypingLabel typingLabel = new TypingLabel(text, textFont);
+    typingLabel.setWrap(true);
+
+    // (IMPORTANT) listener to trigger to the next line
+    typingLabel.setTypingListener(new TypingAdapter() {
+      @Override
+      public void end() {
+        showNextDialogue(); // Recursive call to show consequent lines.
+      }
+    });
+
+    // Add to dialogue table
+    textDialogueTable.add(typingLabel)
+      .growX()
+      .bottom()
+      .left()
+      .padBottom(5f)
+      .row();
+    
+    textDialogueTable.invalidateHierarchy();
+    textDialogueTable.layout();
+  }
+
+  // Dispose memory resources once day1 is done
+  private void dispose() {
+    scenarioTexture.dispose();
   }
 }
--- core/src/main/java/edu/tip/forestoftreasures/View/Day1Screen.java
+++ core/src/main/java/edu/tip/forestoftreasures/View/Day1Screen.java
@@ -6,34 +6,50 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Value;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import edu.tip.forestoftreasures.GameLauncher;
 import edu.tip.forestoftreasures.Controller.Day1Controller;
+import edu.tip.forestoftreasures.utils.DrawableMaker;
 
 public class Day1Screen implements Screen {
-  private Stage stage;
-  private Table table;
-
-  private Skin skin;
+  private final Stage stage;
   private final GameLauncher game;
-
-  private Image settingsIcon;
-
-  private Texture settingsTexture;
   
+  // Assets managed by this screen
+  private final Texture settingsTexture;
+
+  // UI References
+  private Table table;
+  private Image settingsIcon;
+  private Table leftScenarioContentTable;
+  private Table leftDialogueContentTable;
+  private Table topDialogueContentTable;
+  private Table bottomDialogueContentTable;
+
+  // --- Right Panel Color Configuration ---
+  Color rightBorderColor = Color.valueOf("#2a2a2a");
+  Color rightContentColor = Color.valueOf("#212121");
+
+  // --- Left Panel Color Configuration ---
+  Color leftBorderColor = Color.valueOf("#2a2a2a");
+  Color leftContentColor = Color.valueOf("#191a1c");
+
   public Day1Screen(GameLauncher game) {
     this.game = game;
+
+    settingsTexture = new Texture(Gdx.files.internal("icons/Gear.png"));
+    stage = new Stage(new ScreenViewport());
   }
 
   @Override
   public void show() {
     // Prepare your screen here.
-    skin = game.assets.get("ui/fotskin.json", Skin.class);
-    stage = new Stage(new ScreenViewport());
     Gdx.input.setInputProcessor(stage);
 
     // Add actor to stage
@@ -42,30 +58,88 @@ public class Day1Screen implements Screen {
     stage.addActor(table);
 
     // Create a subtable for player stats, skills, and settings of the game
-    Table rightTable = new Table();
-    rightTable.padLeft(50f);
-    rightTable.setDebug(true);
+    // Define right table background and content drawables
+    Drawable rightBorderBg = DrawableMaker.getColoredDrawable(rightBorderColor);
+    Drawable rightContentBg = DrawableMaker.getColoredDrawable(rightContentColor);
 
-    // Adding UI elements to the right table
-    settingsTexture = new Texture(Gdx.files.internal("icons/Gear.png"));
+    // Right Content table
+    Table rightContentTable = new Table();
+    rightContentTable.setBackground(rightContentBg);
+
+    // Right Border table
+    Table rightBorderTable = new Table();
+    rightBorderTable.setBackground(rightBorderBg);
+    rightBorderTable.add(rightContentTable).grow().pad(3f);
+    
+    // Adding UI elements to the right content table
     settingsIcon = new Image(settingsTexture);
-    rightTable.add(settingsIcon)
+    rightContentTable.add(settingsIcon)
       .size(70f)
       .expand()
       .top()
       .right()
       .pad(20f);
 
+    // Create a subtable for game image scenario and text dialogue box
+    // Define left table background and content drawables
+    Drawable leftContentBg = DrawableMaker.getColoredDrawable(leftContentColor);
+    Drawable leftBorderBg = DrawableMaker.getColoredDrawable(leftBorderColor);
+
+    // Table for game image scenario
+    leftScenarioContentTable = new Table();
+    leftScenarioContentTable.setBackground(leftContentBg);
+
+    Table leftScenarioBorderTable = new Table();
+    leftScenarioBorderTable.setBackground(leftBorderBg);
+    leftScenarioBorderTable.add(leftScenarioContentTable).grow().pad(5f);
+
+    // Table for text dialogue box
+    leftDialogueContentTable = new Table();
+    leftDialogueContentTable.setBackground(leftContentBg);
+    leftDialogueContentTable.pad(15f);
+    
+    // Split dialogueContentTable into for text space and interactable widgets.
+    topDialogueContentTable = new Table();
+    topDialogueContentTable.setBackground(leftContentBg);
+    topDialogueContentTable.align(Align.bottomLeft);  // align cells to bottom left of table
+    topDialogueContentTable.pad(0f, 10f, 0f, 10f);
+
+    bottomDialogueContentTable = new Table();
+    bottomDialogueContentTable.setBackground(leftContentBg);
+
+    // Adding top and bottom spaces to the dialogueContentTable
+    leftDialogueContentTable.add(topDialogueContentTable)
+      .grow()
+      .row();
+    leftDialogueContentTable.add(bottomDialogueContentTable)
+      .grow();
+    topDialogueContentTable.setDebug(true);
+
+    Table leftDialogueBorderTable = new Table();
+    leftDialogueBorderTable.setBackground(leftBorderBg);
+    leftDialogueBorderTable.add(leftDialogueContentTable).grow().pad(3f);
+
+    // Table for left panel
+    Table leftContainer = new Table();
+    leftContainer.pad(100f);
+
+    leftContainer.add(leftScenarioBorderTable)
+      .size(500f, 500f)
+      .padBottom(50f)
+      .row();
+    leftContainer.add(leftDialogueBorderTable)
+      .growX()
+      .height(Value.percentHeight(0.4f, leftContainer));
+    
     // Add left table and right table to the main table layout
-    table.add()
-      .expand()
-      .uniformX()
-      .colspan(5);
-    table.add(rightTable)
+    table.add(leftContainer)
       .expand()
       .fill()
-      .uniformX()
-      .colspan(3);
+      .width(Value.percentWidth(0.7f, table));
+    table.add(rightBorderTable)
+      .expand()
+      .fill()
+      .width(Value.percentWidth(0.3f, table));
 
     // Create controller and pass the screen to it
     new Day1Controller(game, this);
@@ -74,7 +148,7 @@ public class Day1Screen implements Screen {
   @Override
   public void render(float delta) {
     // Update and draw your screen here.
-    ScreenUtils.clear(Color.BLACK);
+    ScreenUtils.clear(Color.valueOf("#121315"));
 
     stage.act(delta);
     stage.draw();
@@ -116,5 +190,17 @@ public class Day1Screen implements Screen {
 
   public Image getSettingsIcon() {
     return settingsIcon;
+  }
+
+  public Table getScenarioContentTable() {
+    return leftScenarioContentTable;
+  }
+
+  public Table getTextDialogueTable() {
+    return topDialogueContentTable;
+  }
+
+  public Table getDialogueWidgetTable() {
+    return bottomDialogueContentTable;
   }
 }
